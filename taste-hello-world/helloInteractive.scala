b666d9e0e407bd079b548164bec0d6fafc649c4a
Louis Wust <louis.w.wust@nasa.gov>
2025-10-25 23:51:58 -0400

    Use string interpolation

--- taste-hello-world/helloInteractive.scala
+++ taste-hello-world/helloInteractive.scala
@@ -3,4 +3,4 @@ import scala.io.StdIn.readLine
 @main def helloInteractive() =
   println("Please enter your name:")
   val name = readLine()
-  println("Hello, " + name + "!")
+  println(s"Hello, $name!")
